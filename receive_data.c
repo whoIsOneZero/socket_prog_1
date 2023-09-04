9a574abc4a2ae1dc111593fd02258051e10a880d
whoIsOneZero <oh.shalom.0@gmail.com>
2023-09-04 20:32:00 +0000

    doc: changed ip address. still not working

--- receive_data.c
+++ receive_data.c
@@ -2,13 +2,13 @@
 #include <string.h> //strlen
 #include <sys/socket.h>
 #include <arpa/inet.h> //inet_addr
+#include <unistd.h> //close
 
 int main (int argc, char *argv[])
 {
     int socket_desc;
-    struct sockaddr_in_server;
+    struct sockaddr_in server;
     char *message, server_reply[2000];
-    /*TODO: Make the server_reply have variable length using malloc*/
 
     //CREATE SOCKET
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,7 +18,7 @@ int main (int argc, char *argv[])
         perror("socket");
     }
 
-    server.sin_addr.s_addr = inet_addr("74.125.235.20");
+    server.sin_addr.s_addr = inet_addr("216.58.192.142");
     server.sin_family = AF_INET;
     server.sin_port = htons( 80 );
 
@@ -36,7 +36,8 @@ int main (int argc, char *argv[])
     message = "GET / HTTP/1.1\r\n\r\n";
     if( send(socket_desc, message, strlen(message), 0) < 0)
     {
-        puts("Send failed");
+        perror("send");
+        /*puts("Send failed");*/
 
         return (1);
     }
@@ -44,10 +45,20 @@ int main (int argc, char *argv[])
     puts("Data Sent\n");
 
     //RECEIVE REPLY FROM THE SERVER
+    
+    /*size_t bytes_received;
+    if ((bytes_received = recv(socket_desc, server_reply, sizeof(server_reply) - 1, 0)) < 0)*/
+    
     if( recv(socket_desc, server_reply, 2000, 0) < 0)
     {
-        puts("Receive failed");
+        perror("recv");
+        /*puts("Receive failed");*/
+        
+        return (1);
     }
+    /*TEST THIS:
+    server_reply[bytes_received] = '\0';*/
+
     puts("Reply received\n");
     puts(server_reply);
 
